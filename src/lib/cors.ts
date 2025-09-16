e4769bdc40c14e7abe01e6feb7afd800ff9c37ee
Marcio <marcio.piva@eficazmarketing.com>
2025-09-16 12:08:17 -0300

    Fix: cors

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,8 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function withCORS(response: NextResponse) {
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+export function withCORS(response: NextResponse, req?: NextRequest) {
+  const origin = req?.headers.get("origin");
+
+  if (origin) {
+    response.headers.set("Access-Control-Allow-Origin", origin); 
+    response.headers.set("Vary", "Origin")
+  }
+
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   return response;
 }
